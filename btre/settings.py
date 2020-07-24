0514569581680f7a15b815105e80e583732e9074
gaurav-36 <48172195+gaurav-36@users.noreply.github.com>
2020-07-24 21:55:54 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -147,6 +147,6 @@ MESSAGES_TAGS = {
 # Email Config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'siddhesh.esskay.92@gmail.com'
-EMAIL_HOST_PASSWORD = 'Esskay@92'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Password'
 EMAIL_USE_TLS = True
